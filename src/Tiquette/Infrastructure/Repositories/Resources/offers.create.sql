adab679440d396bb86b106153f5ca220dcf825e7
Boris Guéry <guery.b@gmail.com>
2018-02-01 09:17:26 +0100

    feat(db): Update SQL schemas

---
+++ src/Tiquette/Infrastructure/Repositories/Resources/offers.create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `offers` (
+  `uuid` char(36) NOT NULL DEFAULT '',
+  `proposed_price` int(10) unsigned NOT NULL,
+  `price_currency` char(3) NOT NULL DEFAULT '',
+  `buyer_message` text NOT NULL,
+  `ticket_uuid` char(36) NOT NULL DEFAULT '',
+  `buyer_uuid` char(36) NOT NULL DEFAULT '',
+  PRIMARY KEY (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
